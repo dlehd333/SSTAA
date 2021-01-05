e9d7112896a93ae410d6dff2817857e7b799817c
dlehd333 <dlehd333@gmail.com>
2021-01-05 16:41:31 +0900

    TitleForm 이벤트 추가

--- SSTAA.WinForm/TitleForm.Designer.cs
+++ SSTAA.WinForm/TitleForm.Designer.cs
@@ -69,6 +69,7 @@ namespace SSTAA.WinForm
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "TitleForm";
             this.Text = "TitleForm";
+            this.Click += new System.EventHandler(this.TitleForm_Click);
             this.ResumeLayout(false);
             this.PerformLayout();
 
