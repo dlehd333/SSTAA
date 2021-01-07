0329263edcc75e3991060ddd6028b5aac7e60f19
gun-Daegeungim <daegeungim7301@gmail.com>
2021-01-07 19:14:27 +0900

    방망이 깎던 노인은 방망이를 다 깎았다. 방망방망 서울시 구별 맵 및 각 구별 버튼 추가.

--- SSTAA.WinForm/SelectTownForm.Designer.cs
+++ SSTAA.WinForm/SelectTownForm.Designer.cs
@@ -29,23 +29,17 @@ namespace SSTAA.WinForm
         /// </summary>
         private void InitializeComponent()
         {
-            DevExpress.XtraMap.GeoMapCoordinateSystem geoMapCoordinateSystem1 = new DevExpress.XtraMap.GeoMapCoordinateSystem();
             this.lblDisplayStation = new DevExpress.XtraEditors.LabelControl();
             this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
             this.labelControl3 = new DevExpress.XtraEditors.LabelControl();
-            this.cbxGu = new DevExpress.XtraEditors.ComboBoxEdit();
             this.cbxField = new DevExpress.XtraEditors.ComboBoxEdit();
             this.btnResult = new DevExpress.XtraEditors.SimpleButton();
             this.tablePanel1 = new DevExpress.Utils.Layout.TablePanel();
-            this.TempMapControl = new DevExpress.XtraMap.MapControl();
-            this.imageLayer1 = new DevExpress.XtraMap.ImageLayer();
-            this.bingMapDataProvider1 = new DevExpress.XtraMap.BingMapDataProvider();
+            this.seoulMapControl1 = new SSTAA.WinForm.SeoulMapControl();
             this.textEdit1 = new DevExpress.XtraEditors.TextEdit();
-            ((System.ComponentModel.ISupportInitialize)(this.cbxGu.Properties)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.cbxField.Properties)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.tablePanel1)).BeginInit();
             this.tablePanel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.TempMapControl)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).BeginInit();
             this.SuspendLayout();
             // 
@@ -57,13 +51,9 @@ namespace SSTAA.WinForm
             this.lblDisplayStation.Appearance.Options.UseFont = true;
             this.lblDisplayStation.Appearance.Options.UseTextOptions = true;
             this.lblDisplayStation.Appearance.TextOptions.VAlignment = DevExpress.Utils.VertAlignment.Top;
-            this.tablePanel1.SetColumn(this.lblDisplayStation, 9);
-            this.tablePanel1.SetColumnSpan(this.lblDisplayStation, 3);
             this.lblDisplayStation.Dock = System.Windows.Forms.DockStyle.Fill;
             this.lblDisplayStation.Location = new System.Drawing.Point(760, 86);
             this.lblDisplayStation.Name = "lblDisplayStation";
-            this.tablePanel1.SetRow(this.lblDisplayStation, 2);
-            this.tablePanel1.SetRowSpan(this.lblDisplayStation, 7);
             this.lblDisplayStation.Size = new System.Drawing.Size(239, 477);
             this.lblDisplayStation.TabIndex = 1;
             this.lblDisplayStation.Text = "해당구 역 현황";
@@ -77,12 +67,9 @@ namespace SSTAA.WinForm
             this.labelControl2.Appearance.Options.UseTextOptions = true;
             this.labelControl2.Appearance.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Center;
             this.labelControl2.Appearance.TextOptions.VAlignment = DevExpress.Utils.VertAlignment.Center;
-            this.tablePanel1.SetColumn(this.labelControl2, 3);
-            this.tablePanel1.SetColumnSpan(this.labelControl2, 4);
             this.labelControl2.ImageOptions.Alignment = System.Drawing.ContentAlignment.BottomLeft;
             this.labelControl2.Location = new System.Drawing.Point(219, 21);
             this.labelControl2.Name = "labelControl2";
-            this.tablePanel1.SetRow(this.labelControl2, 1);
             this.labelControl2.Size = new System.Drawing.Size(300, 40);
             this.labelControl2.TabIndex = 3;
             this.labelControl2.Text = "원하는 구를 선택하세요";
@@ -91,45 +78,26 @@ namespace SSTAA.WinForm
             // 
             this.labelControl3.Appearance.BackColor = System.Drawing.Color.White;
             this.labelControl3.Appearance.Options.UseBackColor = true;
-            this.tablePanel1.SetColumn(this.labelControl3, 9);
             this.labelControl3.Location = new System.Drawing.Point(760, 591);
             this.labelControl3.Name = "labelControl3";
-            this.tablePanel1.SetRow(this.labelControl3, 9);
             this.labelControl3.Size = new System.Drawing.Size(41, 18);
             this.labelControl3.TabIndex = 4;
             this.labelControl3.Text = "업종 : ";
             // 
-            // cbxGu
-            // 
-            this.tablePanel1.SetColumn(this.cbxGu, 7);
-            this.cbxGu.Location = new System.Drawing.Point(612, 36);
-            this.cbxGu.Name = "cbxGu";
-            this.cbxGu.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
-            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
-            this.cbxGu.Properties.TextEditStyle = DevExpress.XtraEditors.Controls.TextEditStyles.DisableTextEditor;
-            this.tablePanel1.SetRow(this.cbxGu, 1);
-            this.cbxGu.Size = new System.Drawing.Size(92, 24);
-            this.cbxGu.TabIndex = 5;
-            // 
             // cbxField
             // 
-            this.tablePanel1.SetColumn(this.cbxField, 10);
             this.cbxField.Location = new System.Drawing.Point(809, 588);
             this.cbxField.Name = "cbxField";
             this.cbxField.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
             new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
             this.cbxField.Properties.TextEditStyle = DevExpress.XtraEditors.Controls.TextEditStyles.DisableTextEditor;
-            this.tablePanel1.SetRow(this.cbxField, 9);
             this.cbxField.Size = new System.Drawing.Size(92, 24);
             this.cbxField.TabIndex = 6;
-            this.cbxField.SelectedIndexChanged += new System.EventHandler(this.cbxField_SelectedIndexChanged);
             // 
             // btnResult
             // 
-            this.tablePanel1.SetColumn(this.btnResult, 11);
             this.btnResult.Location = new System.Drawing.Point(907, 585);
             this.btnResult.Name = "btnResult";
-            this.tablePanel1.SetRow(this.btnResult, 9);
             this.btnResult.Size = new System.Drawing.Size(92, 30);
             this.btnResult.TabIndex = 7;
             this.btnResult.Text = "결과보기";
@@ -152,11 +120,10 @@ namespace SSTAA.WinForm
             new DevExpress.Utils.Layout.TablePanelColumn(DevExpress.Utils.Layout.TablePanelEntityStyle.Relative, 10F),
             new DevExpress.Utils.Layout.TablePanelColumn(DevExpress.Utils.Layout.TablePanelEntityStyle.Relative, 10F),
             new DevExpress.Utils.Layout.TablePanelColumn(DevExpress.Utils.Layout.TablePanelEntityStyle.Relative, 2F)});
+            this.tablePanel1.Controls.Add(this.seoulMapControl1);
             this.tablePanel1.Controls.Add(this.textEdit1);
-            this.tablePanel1.Controls.Add(this.TempMapControl);
             this.tablePanel1.Controls.Add(this.btnResult);
             this.tablePanel1.Controls.Add(this.cbxField);
-            this.tablePanel1.Controls.Add(this.cbxGu);
             this.tablePanel1.Controls.Add(this.labelControl3);
             this.tablePanel1.Controls.Add(this.labelControl2);
             this.tablePanel1.Controls.Add(this.lblDisplayStation);
@@ -179,31 +146,21 @@ namespace SSTAA.WinForm
             this.tablePanel1.Size = new System.Drawing.Size(1022, 720);
             this.tablePanel1.TabIndex = 0;
             // 
-            // TempMapControl
+            // seoulMapControl1
             // 
-            this.TempMapControl.CenterPoint = new DevExpress.XtraMap.GeoPoint(37.5D, 127D);
-            this.tablePanel1.SetColumn(this.TempMapControl, 1);
-            this.tablePanel1.SetColumnSpan(this.TempMapControl, 7);
-            geoMapCoordinateSystem1.CircularScrollingMode = DevExpress.XtraMap.CircularScrollingMode.TilesAndVectorItems;
-            this.TempMapControl.CoordinateSystem = geoMapCoordinateSystem1;
-            this.TempMapControl.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.TempMapControl.Layers.Add(this.imageLayer1);
-            this.TempMapControl.Location = new System.Drawing.Point(23, 86);
-            this.TempMapControl.Name = "TempMapControl";
-            this.tablePanel1.SetRow(this.TempMapControl, 2);
-            this.tablePanel1.SetRowSpan(this.TempMapControl, 8);
-            this.TempMapControl.Size = new System.Drawing.Size(681, 546);
-            this.TempMapControl.TabIndex = 8;
-            this.TempMapControl.ZoomLevel = 11D;
-            this.imageLayer1.DataProvider = this.bingMapDataProvider1;
-            this.bingMapDataProvider1.BingKey = "AldT8DQXZMZIN37VvXh-4jIb4GaOdsG1ui_Y6S8dAdueZ4s94BmJwkarIOg8R17K";
+            this.tablePanel1.SetColumn(this.seoulMapControl1, 1);
+            this.tablePanel1.SetColumnSpan(this.seoulMapControl1, 7);
+            this.seoulMapControl1.Location = new System.Drawing.Point(23, 86);
+            this.seoulMapControl1.Name = "seoulMapControl1";
+            this.tablePanel1.SetRow(this.seoulMapControl1, 2);
+            this.tablePanel1.SetRowSpan(this.seoulMapControl1, 8);
+            this.seoulMapControl1.Size = new System.Drawing.Size(681, 546);
+            this.seoulMapControl1.TabIndex = 10;
             // 
             // textEdit1
             // 
-            this.tablePanel1.SetColumn(this.textEdit1, 10);
             this.textEdit1.Location = new System.Drawing.Point(809, 36);
             this.textEdit1.Name = "textEdit1";
-            this.tablePanel1.SetRow(this.textEdit1, 1);
             this.textEdit1.Size = new System.Drawing.Size(92, 24);
             this.textEdit1.TabIndex = 9;
             // 
@@ -218,12 +175,10 @@ namespace SSTAA.WinForm
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "SelectTownForm";
             this.Text = "SelectTownForm";
-            ((System.ComponentModel.ISupportInitialize)(this.cbxGu.Properties)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.cbxField.Properties)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.tablePanel1)).EndInit();
             this.tablePanel1.ResumeLayout(false);
             this.tablePanel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.TempMapControl)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -234,14 +189,11 @@ namespace SSTAA.WinForm
 
         private DevExpress.XtraEditors.LabelControl lblDisplayStation;
         private DevExpress.Utils.Layout.TablePanel tablePanel1;
-        private DevExpress.XtraMap.MapControl TempMapControl;
         private DevExpress.XtraEditors.SimpleButton btnResult;
         private DevExpress.XtraEditors.ComboBoxEdit cbxField;
-        private DevExpress.XtraEditors.ComboBoxEdit cbxGu;
         private DevExpress.XtraEditors.LabelControl labelControl3;
         private DevExpress.XtraEditors.LabelControl labelControl2;
-        private DevExpress.XtraMap.ImageLayer imageLayer1;
-        private DevExpress.XtraMap.BingMapDataProvider bingMapDataProvider1;
         private DevExpress.XtraEditors.TextEdit textEdit1;
+        private SeoulMapControl seoulMapControl1;
     }
 }
---
+++ SSTAA.WinForm/SeoulMapControl.cs
@@ -0,0 +1,89 @@
+﻿using DevExpress.XtraEditors;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SSTAA.WinForm
+{
+    public partial class SeoulMapControl : DevExpress.XtraEditors.XtraUserControl
+    {
+        public SeoulMapControl()
+        {
+            InitializeComponent();
+        }
+        #region ButtonClicked event things for C# 3.0
+        public event EventHandler<ButtonClickedEventArgs> ButtonClicked;
+
+        protected virtual void OnButtonClicked(ButtonClickedEventArgs e)
+        {
+            if (ButtonClicked != null)
+                ButtonClicked(this, e);
+        }
+
+        private ButtonClickedEventArgs OnButtonClicked(int districtNumber)
+        {
+            ButtonClickedEventArgs args = new ButtonClickedEventArgs(districtNumber);
+            OnButtonClicked(args);
+
+            return args;
+        }
+
+        private ButtonClickedEventArgs OnButtonClickedForOut()
+        {
+            ButtonClickedEventArgs args = new ButtonClickedEventArgs();
+            OnButtonClicked(args);
+
+            return args;
+        }
+
+        public class ButtonClickedEventArgs : EventArgs
+        {
+            public int DistrictNumber { get; set; }
+
+            public ButtonClickedEventArgs()
+            {
+            }
+
+            public ButtonClickedEventArgs(int districtNumber)
+            {
+                DistrictNumber = districtNumber;
+            }
+        }
+        #endregion
+
+        private void OccurEvent(object sender)
+        {
+            SimpleButton btn = sender as SimpleButton;
+            InitializeButtonColor();
+            btn.Appearance.BackColor = System.Drawing.Color.Transparent;
+            ButtonClickedEventArgs args = new ButtonClickedEventArgs();
+            args.DistrictNumber = Convert.ToInt32(btn.Tag);
+            OnButtonClicked(args.DistrictNumber);
+        }
+
+        private void InitializeButtonColor()
+        {
+            foreach (var control in Controls)
+            {
+                if (control.GetType() == btnYongsanGu.GetType())
+                    ((SimpleButton)control).Appearance.BackColor = Color.SteelBlue;
+            }
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            OccurEvent(sender);
+        }
+
+        private void pictureEdit1_Click(object sender, EventArgs e)
+        {
+            InitializeButtonColor();
+        }
+    }
+}
